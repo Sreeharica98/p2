dee29ab5b44a3d63e432db8c3e636669da78a5a9
Sreeharica98 <sreeharica98@gmail.com>
2025-03-14 18:36:16 +0530

    imgscript.js

--- ImageSliderProject/imgscript.js
+++ ImageSliderProject/imgscript.js
@@ -10,23 +10,15 @@ let currentIndex = 0;
 const sliderImage = document.getElementById("sliderImage");
 const nextButton = document.getElementById("nextButton");
 const prevButton = document.getElementById("prevButton");
-
-// Function to update the displayed image
 function updateImage() {
     sliderImage.src = images[currentIndex];
 }
-
-// Next Button Logic
 nextButton.addEventListener("click", () => {
     currentIndex = (currentIndex + 1) % images.length;
     updateImage();
 });
-
-// Previous Button Logic
 prevButton.addEventListener("click", () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage();
 });
-
-// Initial Image Display
 updateImage();
